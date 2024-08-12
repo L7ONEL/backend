84e471ed294917cefecd0952b92286caa3822004
L7ONEL <lionelardusso4730@gmail.com>
2024-08-12 20:31:19 -0300

    first commit

---
+++ api/personas.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/", function(req, res, next){
+    res.send("API PERSONAS")
+});
+
+router.get('/123', function(req, res, next){
+    res.send("Ruta de 123");
+});
+
+router.get('/:id', function(req, res, next){
+    res.send(`Ruta de persona id ${req.params.id}`);
+});
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const personasRouter = require('./api/personas')
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", function(req, res, next){
+    res.send("App express")
+})
+
+app.get("/a", function(req, res, next){
+    res.send("otra ruta")
+})
+
+app.use('/api/personas', personasRouter);
+
+app.listen(port, () => {
+    console.log(`Ejecutando servidor ${port}`);
+})
