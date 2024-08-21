04ec6a61cb860b72dc2264472ca749276de3f49b
L7ONEL <lionelardusso4730@gmail.com>
2024-08-21 18:06:24 -0300

    Estuvimos viendo mas teoría y vimos como funciona el get, post, put, delete.

--- api/personas.js
+++ api/personas.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
+/*
+PARAMS
+    - query     ?id=123&nombre=ejemplo
+    - params    /123/ejemplo
+    
+BODY
+    - body  { "id": 123, "nombre": "Ejemplo" }
+
+        ACCIÓN      SQL     HTTP        DATOS
+C  =>   CREATE     INSERT   POST        
+R  =>   READ       SELECT   GET         PARAMS
+U  =>   UPDATE     UPDATE   PUT
+D  =>   DELETE     DELETE   DELETE
+
+RECIBE    => req => query, params, body, headers
+RESPUESTA => res => status, send, sendFile, json, download, render
+*/
+
 router.get("/", function(req, res, next){
+    //OBTENER TODAS LAS PERSONAS
+
+    const { edad, nombre, apellido } = req.query;
+
+    console.log({edad, nombre, apellido});
+    
+
     res.send("API PERSONAS")
 });
 
@@ -10,7 +35,37 @@ router.get('/123', function(req, res, next){
 });
 
 router.get('/:id', function(req, res, next){
+    //OBTENER UNA PERSONA X ID
     res.send(`Ruta de persona id ${req.params.id}`);
 });
+
+router.post("/", function(req, res, next){
+    //GUARDAR 1 PERSONA (TODOS SUS DATOS)
+
+    //datos de persona: documento, nombres, apellidos, direccion
+    const { documento, nombres, apellidos, direccion } = req.body;
+
+    console.log({documento, nombres, apellidos, direccion});
+
+    res.json({
+        status: "ok",
+        id: 123
+    })
+    
+})
+
+router.put("/", function(req, res, next){
+    //ACYUALIZAR 1 PERSONA
+
+    const { id } = req.query;
+    const { documento, nombres, apellidos, direccion } = req.body;
+
+})
+
+router.delete("/", function(req, res, next){
+    //ELIMINAR UNA PERSONA
+
+    const { id } = req.query;
+})
 
 module.exports = router;
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/", function(req, res, next){
     res.send("App express")
 })
